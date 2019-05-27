468b57449b1b72acd435fd1d2d06b930e18369c4
Karl Fritsche <github@attrib.org>
2019-05-27 21:24:30 +0200

    Added admin user detail route

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { compose } from 'recompose';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import { withFirebase } from '../Firebase';
 import { withAuthorization } from '../Session';
 import * as ROLES from '../../constants/roles';
+import * as ROUTES from '../../constants/routes';
 
-class AdminPage extends Component {
+const AdminPage = () => (
+  <div>
+    <h1>Admin</h1>
+    <p>The Admin Page is accessible by every signed in admin user.</p>
+    <Switch>
+      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+      <Route exact path={ROUTES.ADMIN} component={UserList} />
+    </Switch>
+  </div>
+);
+
+class UserListBase extends Component {
   constructor(props) {
     super(props);
 
@@ -41,37 +54,108 @@ class AdminPage extends Component {
 
     return (
       <div>
-        <h1>Admin</h1>
-        <p>
-          The Admin Page is accessible by every signed in admin user.
-        </p>
-
+        <h2>Users</h2>
         {loading && <div>Loading ...</div>}
 
-        <UserList users={users} />
+        <ul>
+          {users.map(user => (
+            <li key={user.uid}>
+        <span>
+          <strong>ID:</strong> {user.uid}
+        </span>
+              <span>
+          <strong>Username:</strong> {user.username}
+        </span>
+              <span>
+          <strong>Roles: </strong>
+          <Roles roles={user.roles} />
+        </span>
+          <span>
+<Link
+  to={{
+    pathname: `${ROUTES.ADMIN}/${user.uid}`,
+    state: { user },
+  }}
+>
+  Details
+            </Link>
+          </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID:</strong> {user.uid}
-        </span>
-        <span>
-          <strong>Username:</strong> {user.username}
-        </span>
-        <span>
-          <strong>Roles: </strong>
-          <Roles roles={user.roles} />
-        </span>
-      </li>
-    ))}
-  </ul>
-);
+const UserList = withFirebase(UserListBase);
+
+class UserItemBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      user: null,
+    };
+  }
+  componentDidMount() {
+    if (this.state.user) {
+      return;
+    }
+
+    this.setState({ loading: true });
+    this.props.firebase
+      .user(this.props.match.params.id)
+      .get()
+      .then(doc => {
+        this.setState({
+          user: {
+            uid: doc.id,
+            ...doc.data(),
+          },
+          loading: false,
+        });
+      });
+  }
+
+  onSendPasswordResetEmail = () => {
+    this.props.firebase.doPasswordReset(this.state.user.email);
+  };
+
+
+  render() {
+    const { user, loading } = this.state;
+    return (
+      <div>
+        <h2>User ({this.props.match.params.id})</h2>
+        {loading && <div>Loading ...</div>}
+        {user && (
+          <div>
+            <span>
+            <strong>ID:</strong> {user.uid}
+            </span>
+                        <span>
+            <strong>E-Mail:</strong> {user.email}
+            </span>
+                        <span>
+            <strong>Username:</strong> {user.username}
+            </span>
+            <span>
+            <button
+              type="button"
+              onClick={this.onSendPasswordResetEmail}
+            >
+            Send Password Reset
+            </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+const UserItem = withFirebase(UserItemBase);
 
 const Roles = ({ roles }) => (
   <ul>
