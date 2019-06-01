28277ef2451ef2858212f87a27e8ed165466a84f
Karl Fritsche <github@attrib.org>
2019-06-01 22:23:11 +0200

    Add google functions to render markdown on News update

---
+++ functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions')
+const md = require('markdown-it')();
+
+exports.renderMarkdown = functions.firestore
+  .document('News/{newsId}')
+  .onWrite((change, context) => {
+    // Retrieve the current and previous value
+    const data = change.after.exists ? change.after.data() : null;
+    const previousData = change.before.data();
+
+    // This is crucial to prevent infinite loops.
+    if (!data || !data.Markup || data.Markdown === previousData.Markdown) return null;
+
+    return change.after.ref.set({
+      Content: md.render(data.Markdown)
+    }, {merge: true});
+  });
