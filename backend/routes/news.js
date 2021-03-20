73c8224ad00b5b2662da44388f0075e632a16cae
Karl Fritsche <github@attrib.org>
2021-03-20 15:53:56 +0100

    Allow deleting of news

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -8,13 +8,13 @@ const express = require('express'),
 const md = new MarkdownIt()
 
 function prepareNews(news) {
-  news.content = md.render(news.markdown)
+  news.content = md.render(news.markdown ?? '')
   return news
 }
 
 router.get('/', (req, res) => {
   const parameters = {}
-  const query = "MATCH (news:News) RETURN news ORDER BY news.date"
+  const query = "MATCH (news:News) RETURN news ORDER BY news.date DESC"
   runQuery(res, query, parameters)
     .then((result) => {
       res.send(result.map(record => prepareNews(record.news)))
@@ -61,6 +61,20 @@ router.patch('/:id', acl('admin'), (req, res) => {
       res.send({error: 'Not found'})
     }
   })
+})
+
+router.delete('/:id', acl('admin'), (req, res) => {
+  const parameter = {id: neo4j.types.Integer.fromString(req.params.id)}
+  let query = 'MATCH (news:News) WHERE ID(news)=$id DETACH DELETE news'
+  runQuery(res, query, parameter)
+    .then(() => {
+      res.send({success: true})
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(400)
+      res.send({error})
+    })
 })
 
 module.exports = router
