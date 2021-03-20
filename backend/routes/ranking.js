e6d1bea9c7b7aa11b19c54cee5016e37e5ce885d
Karl Fritsche <github@attrib.org>
2021-03-20 20:58:19 +0100

    Fix credit sum not calcualted correctly

--- backend/routes/ranking.js
+++ backend/routes/ranking.js
@@ -22,7 +22,7 @@ router.get('/', (req, res) => {
   const rankBy = req.query.by === 'game' ? 'ID(game) AS id, game.name AS name' : 'ID(player) AS id, player.nick AS nick';
   const query = 'MATCH (player:Player)--(:Team)-[score:SCORED]-(result:Result)--(game:Game), (result)--(tag:Tag) ' +
     (filter.length > 0 ? 'WHERE ' + filter.join(' AND ') + ' ' : '') +
-    'RETURN ' + rankBy + ', AVG(score.funkies) AS funkies, SUM(score.funkies)-COUNT(score) AS funkyDiff, SUM(score.won) AS won, COUNT(score) AS played, (SUM(score.won)/COUNT(score)*100) AS wonPercentage ' +
+    'RETURN ' + rankBy + ', AVG(score.funkies) AS funkies, SUM(score.funkies)-COUNT(score)+1 AS funkyDiff, SUM(score.won) AS won, COUNT(score) AS played, (SUM(score.won)/COUNT(score)*100) AS wonPercentage ' +
     'ORDER BY ' + order + ' DESC';
   runQuery(res, query, parameters)
     .then((result) => {
